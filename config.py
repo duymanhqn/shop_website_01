04a8f15c0e706f9acf0c3398a154bde887aab5ab
Manh <phamduymanh7931@gmail.com>
2025-11-14 22:22:06 +0700

    ...

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-class Config:
-    #  Kết nối đến PostgreSQL hiện tại của bạn (shop_db)
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123456@localhost:5432/shop_db"
-
-    #  Tắt track modification để tránh warning
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-# import os
-
 # class Config:
-#     SQLALCHEMY_DATABASE_URI = "postgresql://shop:EbwomBvht9ZjpqrnCbmjKbLz2udlK0EN@dpg-d46sdpk9c44c738n5aj0-a/shop_db_ak4d"
+#     #  Kết nối đến PostgreSQL hiện tại của bạn (shop_db)
+#     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123456@localhost:5432/shop_db"
+
+#     #  Tắt track modification để tránh warning
 #     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = "postgresql://shop:EbwomBvht9ZjpqrnCbmjKbLz2udlK0EN@dpg-d46sdpk9c44c738n5aj0-a/shop_db_ak4d"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
