0354973111ef8da03ff05e71c93266fe89f649bc
Manh <phamduymanh7931@gmail.com>
2025-11-14 11:52:25 +0700

    test api gmail render

--- utils/send_email.py
+++ utils/send_email.py
@@ -1,20 +1,44 @@
-import smtplib
-from threading import Thread
-from email.mime.text import MIMEText
+# import smtplib
+# from threading import Thread
+# from email.mime.text import MIMEText
 
-SENDER_EMAIL = "doanthanhtuyen0330@gmail.com"
-SENDER_APP_PASSWORD = "qykm zzuf lofo aumf"
+# SENDER_EMAIL = "doanthanhtuyen0330@gmail.com"
+# SENDER_APP_PASSWORD = "qykm zzuf lofo aumf"
+
+# def _send(email, otp):
+#     msg = MIMEText(f"OTP của bạn: {otp}\nHiệu lực 5 phút.", "plain", "utf-8")
+#     msg["Subject"] = "Xác nhận OTP"
+#     msg["From"] = SENDER_EMAIL
+#     msg["To"] = email
+
+#     server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+#     server.login(SENDER_EMAIL, SENDER_APP_PASSWORD)
+#     server.send_message(msg)
+#     server.quit()
+
+# def send_otp_email(email, otp):
+#     Thread(target=_send, args=(email, otp)).start()
+from threading import Thread
+import resend
+
+# API key để trực tiếp trong code (bạn yêu cầu như vậy)
+resend.api_key = "re_5w9mHaod_KwdbayCgcRgSbAXHBdx8ScDp"
+
 
 def _send(email, otp):
-    msg = MIMEText(f"OTP của bạn: {otp}\nHiệu lực 5 phút.", "plain", "utf-8")
-    msg["Subject"] = "Xác nhận OTP"
-    msg["From"] = SENDER_EMAIL
-    msg["To"] = email
+    params = {
+        "from": "OTP Service <onboarding@resend.dev>",
+        "to": email,
+        "subject": "Mã OTP Xác Nhận",
+        "text": f"OTP của bạn là: {otp}\nHiệu lực 5 phút.",
+    }
 
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server.login(SENDER_EMAIL, SENDER_APP_PASSWORD)
-    server.send_message(msg)
-    server.quit()
+    try:
+        resend.Emails.send(params)
+    except Exception as e:
+        print("Lỗi gửi email:", e)
+
 
 def send_otp_email(email, otp):
+    # Gửi email trong thread để không làm chậm request
     Thread(target=_send, args=(email, otp)).start()
