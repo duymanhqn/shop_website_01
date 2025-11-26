6c3de788cd6417fad555900de437b65a615f3c19
Manh <phamduymanh7931@gmail.com>
2025-11-26 19:40:11 +0700

    full

--- utils/payment_helper.py
+++ utils/payment_helper.py
@@ -1,25 +1,4 @@
-# # utils/payment_helper.py
-# from flask import request, url_for
-# from urllib.parse import urljoin
 
-# def get_callback_url(endpoint):
-#     """
-#     Tạo URL callback động cho mọi môi trường:
-#     - localhost → http://localhost:5000
-#     - Render → https://htmthshop.onrender.com
-#     - Production → https://mhtmh.id.vn
-#     """
-#     host = request.host  # Ví dụ: localhost:5000, htmthshop.onrender.com, mhtmh.id.vn
-
-#     # Xác định scheme (http hay https)
-#     if host.startswith('localhost') or host.startswith('127.0.0.1'):
-#         scheme = 'http'
-#     else:
-#         scheme = 'https'
-
-#     base_url = f"{scheme}://{host}"
-#     return urljoin(base_url + '/', url_for(endpoint))
-# utils/payment_helper.py
 from flask import request, url_for
 from urllib.parse import urljoin
 
